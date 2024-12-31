c3d85610ec71a193cd01e02bd1406d4d722bd76b
kk2425 <166939325+kk2425@users.noreply.github.com>
2024-12-31 23:16:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#69247C',
+      },
+      fontFamily: {
+        'display': ['Poppins','sans-serif'],
+        'body':['Inter','sans-serif'],
+      },
+    },
+  },
+  variants:{},
+  plugins: [],
+}
+
